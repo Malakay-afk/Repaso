848ea098598248d0197de576235c7a8b9673b0d9
Malakay-afk <mikelrapsolo@gmail.com>
2022-03-16 20:17:49 +0100

    examen

--- Repaso_1.php
+++ Repaso_1.php
@@ -11,24 +11,32 @@
     <form action="Repaso_1.php" method="get">
     <input type="text" name="campo1">
     <input type="text" name="campo2">
-    <input type="submit" value="Enviar">    
+    <input type="submit" value="Sumar" name="sumar"/>
+    <input type="submit" value="Restar" name="restar"/>
+    <input type="submit" value="Multiplicar" name="multiplicar"/>
+    <input type="submit" value="Dividir" name="dividir"/>   
     </form>
 <?php
 
      $n1 = $_GET['campo1'];
      $n2 = $_GET['campo2'];
+
        $multiplicacion=$n1*$n2;
        $resta=$n1-$n2;
        $dividir=$n1/$n2;
        $suma=$n1+$n2;
-        echo "la multiplicación tiene como resultado: $multiplicacion"."</br>";
-        echo "la división tiene como resultado: $dividir"."</br>";
-        echo "la resta tiene como resultado: $resta"."</br>";
-        echo "la suma tiene como resultado: $suma"."</br>";
-        
 
+if (isset($_REQUEST['sumar'])) {
+    echo "la suma tiene como resultado: $suma"."</br>";
+}if (isset($_REQUEST['restar'])) {
+    echo "la resta tiene como resultado: $resta"."</br>";
+}if (isset($_REQUEST['multiplicar'])){
+    echo "la multiplicación tiene como resultado: $multiplicacion"."</br>";
+}if (isset($_REQUEST['dividir'])) {
+    echo "la división tiene como resultado: $dividir"."</br>";
+}
 
-        
+                
     ?>
 </body>
 </html>
